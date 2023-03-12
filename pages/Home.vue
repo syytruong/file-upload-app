866b37b8355cf56530e128ccd5a3bf175331f63f
Sy Truong <sy.truong@trackinno.com>
2023-03-12 11:21:48 +0200

    fix token expired -- change to 1 month exp time, change data return for APIs, add Css for reponsive UI

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,33 +1,76 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold mb-4">Upload File</h1>
-    <input type="file" ref="fileInput" @change="uploadFile" accept=".pdf">
-    <div class="mt-4" v-if="uploadedFiles.length > 0">
-      <h2 class="text-2xl font-bold mb-4">Uploaded Files</h2>
-      <ul>
-        <li v-for="file in uploadedFiles" :key="file">{{ file }}</li>
-      </ul>
+  <div class="file-uploader">
+    <div class="file-picker">
+      <h1 style="margin-bottom: 20px">Upload File Applications</h1>
+      <div class="file-input">
+        <input type="file" ref="fileInput" @change="handleFileSelected">
+        <button class="upload-btn" @click="handleUpload" :disabled="isUploading">Upload</button>
+      </div>
+    </div>
+    <div class="file-list">
+      <table>
+        <thead>
+          <tr>
+            <th>File Name</th>
+            <th>File Size</th>
+            <th>File Type</th>
+            <th>Created Date</th>
+          </tr>
+        </thead>
+        <tbody v-if="uploadedFiles.length > 0">
+          <tr v-for="file in uploadedFiles" :key="file.name">
+            <td>{{ file.name }}</td>
+            <td>{{ getFileSize(file.size) }}</td>
+            <td>{{ getFileType(file.name) }}</td>
+            <td>{{ getFileCreatedDate(file.createdDate) }}</td>
+          </tr>
+        </tbody>
+        <tbody v-else>
+          <tr>
+            <td colspan="12">
+              <h2 style="text-align: center;">No file uploaded!</h2>
+            </td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
+
 <script>
 import axios from 'axios';
+import path from 'path';
 
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNjc4NjA1Mzg0LCJleHAiOjE2Nzg2MDkwMTR9.uxwUk60nuOgMGvzZuIC5v0S19-YKKFnxOPKRC2YAhYE';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE2Nzg2MTE0NjUsImV4cCI6MTY4MTIwMzQ5NX0.ndrKyHcKmOY1_OBNLc_862BuLd8OiofaALkb1F_P-_I';
 
 export default {
   name: 'Home',
   data() {
     return {
       uploadedFiles: [],
+      selectedFile: null,
+      isUploading: false,
     };
   },
   methods: {
-    uploadFile() {
+    handleFileSelected(event) {
+      console.log("WHAT ??", event.target.files[0])
+      this.selectedFile = event.target.files[0];
+    },
+    handleUpload() {
+      if (!this.selectedFile) {
+        alert('Please select a file');
+        return;
+      }
+      if (this.isUploading) {
+        return;
+      }
+
+      this.isUploading = true;
       const formData = new FormData();
-      const file = this.$refs.fileInput.files[0];
-      formData.append('file', file);
+      formData.append('file', this.selectedFile);
+      this.isUploading = true;
       axios
         .post('http://localhost:5001/upload', formData, {
           headers: {
@@ -36,14 +79,13 @@ export default {
           },
         })
         .then((response) => {
-          console.log(response.data);
-          this.uploadedFiles.push(file.name);
-          this.$refs.fileInput.value = '';
-          alert('File uploaded successfully');
+          this.uploadedFiles.push(response.data.file);
+          this.selectedFile = null;
         })
         .catch((error) => {
           console.log(error);
-          alert('Failed to upload file');
+        }).finally(() => {
+          this.isUploading = false
         });
     },
     fetchFiles() {
@@ -54,15 +96,137 @@ export default {
           }
         })
         .then((response) => {
+          console.log(response.data)
           this.uploadedFiles = response.data.files;
         })
         .catch((error) => {
           console.log(error);
         })
     },
+    getFileSize(size) {
+      if (size === 0) return '0 Bytes';
+      const k = 1024;
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+      const i = Math.floor(Math.log(size) / Math.log(k));
+      const fileSize = parseFloat((size / Math.pow(k, i)).toFixed(2));
+      return `${fileSize} ${sizes[i]}`;
+    },
+    getFileType(filename) {
+      return path.extname(filename).substring(1).toUpperCase();
+    },
+    getFileCreatedDate(createdDate) {
+      const date = new Date(createdDate);
+      const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June', 'July',
+        'August', 'September', 'October', 'November', 'December'
+      ];
+      const year = date.getFullYear();
+      const month = monthNames[date.getMonth()];
+      const day = date.getDate();
+      return `${month} ${day}, ${year}`;
+    }
   },
   mounted() {
     this.fetchFiles();
   },
 };
 </script>
+
+<style>
+  .file-uploader {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: column;
+    width: 100%;
+    margin-top: 0;
+    margin-bottom: auto;
+  }
+
+.file-picker {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 10%;
+  margin-bottom: 30px;
+  width: 100%;
+}
+
+.file-picker h1 {
+  font-size: 36px;
+  font-weight: bold;
+}
+
+.file-input {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.file-input input[type="file"] {
+  margin-right: 10px;
+  font-size: 16px;
+  padding: 10px;
+  width: 300px;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+}
+
+.file-input .upload-btn {
+  background-color: #008CBA;
+  border: none;
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  border-radius: 10px;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.file-input .upload-btn:hover {
+  background-color: #0077B3;
+}
+
+.file-input .upload-btn:disabled {
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+
+.file-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+
+.file-list h2 {
+  font-size: 28px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.file-list table {
+  border-collapse: collapse;
+  margin-top: 20px;
+  width: 100%;
+}
+
+.file-list table th,
+.file-list table td {
+  border: 1px solid #ddd;
+  padding: 10px;
+  text-align: center;
+}
+
+.file-list table th {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.file-list table td {
+  font-size: 16px;
+}
+</style>
--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const { body } = require('express-validator');
 const helmet = require('helmet');
+const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'jwt-secret123';
 const USERS = [
@@ -16,9 +17,13 @@ const USERS = [
     id: '1234567890',
     name: 'John Doe',
     iat: Math.floor(Date.now() / 1000) - 30,
-    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 30), // 1 month valid
   }
 ];
+
+// const token = jwt.sign(USERS[0], JWT_SECRET);
+// console.log(token);
+
 const UPLOADS_DIR = path.join(__dirname, 'uploads');
 if (!fs.existsSync(UPLOADS_DIR)) {
   fs.mkdirSync(UPLOADS_DIR);
@@ -37,7 +42,7 @@ passport.use(new JwtStrategy(jwtOptions, async (payload, done) => {
 
     // TODO: connect with DB later
     // const user = await User.findById(payload.sub);
-    const user = USERS.find(u => u.id === payload.sub);
+    const user = USERS.find(u => u.id === payload.id);
 
     if (!user) {
       return done(null, false);
@@ -79,7 +84,20 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post('/upload', uploadFileValidation, upload.single('file'), (req, res) => {
-  res.send({ message: 'File uploaded successfully' });
+  if (!req.file) {
+    return res.status(400).send({ message: 'No file uploaded' });
+  }
+
+  const { originalname, size, lastModifiedDate } = req.file;
+
+  const file = {
+    name: originalname,
+    url: `http://localhost:5001/uploads/${originalname}`,
+    size,
+    createdDate: lastModifiedDate ? lastModifiedDate : new Date(),
+  };
+
+  res.send({ message: 'File uploaded successfully', file });
 });
 
 app.get('/files', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -101,7 +119,7 @@ app.get('/files', passport.authenticate('jwt', { session: false }), (req, res) =
         name: filename,
         url: `http://localhost:5001/uploads/${filename}`,
         size: fileStats.size,
-        createdDate: fileStats.birthtime
+        createdDate: fileStats.birthtime,
       };
     });
     res.send({ files: fileList });
